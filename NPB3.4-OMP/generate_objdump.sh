e42e828e07d49967fb02073f27226ab1ec394a26
Hoa Nguyen <hn@hnpl.org>
2026-05-11 23:52:06 +0000

    Add script to generate objdumps
    Signed-off-by: Hoa Nguyen <hn@hnpl.org>

---
+++ NPB3.4-OMP/generate_objdump.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+objdump -j .text -D bin/is.S.x.sampling.m5.pdev > is.S.sampling.objdump
+objdump -j .text -D bin/is.D.x.sampling.m5.pdev > is.D.sampling.objdump
+objdump -j .text -D bin/cg.S.x.sampling.m5.pdev > cg.S.sampling.objdump
+objdump -j .text -D bin/cg.E.x.sampling.m5.pdev > cg.E.sampling.objdump
