5c766973bb573d11064a3ad8c6c6d9a1c9ead398
Hoa Nguyen <hn@hnpl.org>
2026-05-13 08:55:06 +0000

    Fix error
    Signed-off-by: Hoa Nguyen <hn@hnpl.org>

--- NPB3.4-OMP/generate_sampling_points.sh
+++ NPB3.4-OMP/generate_sampling_points.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+cd /workdir/NPB/NPB3.4-OMP/
 python3 generate_sampling_points.py --output_dir=/workdir/experiments/prefetcher/gem5_configurations/npb_sampling_points/ --workload cg.E --sampling_site=1
 python3 generate_sampling_points.py --output_dir=/workdir/experiments/prefetcher/gem5_configurations/npb_sampling_points/ --workload cg.E --sampling_site=2
 python3 generate_sampling_points.py --output_dir=/workdir/experiments/prefetcher/gem5_configurations/npb_sampling_points/ --workload is.D --sampling_site=1
