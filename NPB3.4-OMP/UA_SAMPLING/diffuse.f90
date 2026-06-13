94087e4564361817e75e1e68db4a27c273e01ae0
Hoa Nguyen <hn@hnpl.org>
2026-06-13 03:16:29 -0700

    Remove trailing spaces
    Signed-off-by: Hoa Nguyen <hn@hnpl.org>

--- NPB3.4-OMP/UA_SAMPLING/diffuse.f90
+++ NPB3.4-OMP/UA_SAMPLING/diffuse.f90
@@ -1,5 +1,5 @@
 !---------------------------------------------------------------------
-      subroutine diffusion(ifmortar)      
+      subroutine diffusion(ifmortar)
 !---------------------------------------------------------------------
 !     advance the diffusion term using CG iterations
 !---------------------------------------------------------------------
@@ -63,7 +63,7 @@
 !.................................................................
 
       do iter=1, nmxh
-        if(iter.gt.1) then 
+        if(iter.gt.1) then
           rho_aux = 0.d0
 !$OMP PARALLEL DEFAULT(SHARED) PRIVATE(im,ie,i,j,k) REDUCTION(+:rho_aux)
 !$OMP DO
@@ -95,28 +95,28 @@
           beta = rho1/rho2
 !.........update p_m+1 in the specification
           call adds1m1(pdiff, pdiffp, beta,ntot)
-          call adds1m1(pmorx, ppmor,  beta, nmor)  
+          call adds1m1(pmorx, ppmor,  beta, nmor)
         end if
- 
+
 !.......compute matrix vector product: (theta pm) in the specification
 
         if (timeron) call timer_start(t_transf)
-        call transf(pmorx,pdiff) 
+        call transf(pmorx,pdiff)
         if (timeron) call timer_stop(t_transf)
 
 !.......compute pdiffp which is (A theta pm) in the specification
-!$OMP PARALLEL DO DEFAULT(SHARED) PRIVATE(ie) 
+!$OMP PARALLEL DO DEFAULT(SHARED) PRIVATE(ie)
         do ie=1, nelt
           call laplacian(pdiffp(1,1,1,ie),pdiff(1,1,1,ie),size_e(ie))
         end do
 !$OMP END PARALLEL DO
 
-!.......compute ppmor which will be used to compute (thetaT A theta pm) 
+!.......compute ppmor which will be used to compute (thetaT A theta pm)
 !       in the specification
         if (timeron) call timer_start(t_transfb)
-        call transfb(ppmor,pdiffp) 
+        call transfb(ppmor,pdiffp)
         if (timeron) call timer_stop(t_transfb)
- 
+
 !.......apply boundary condition
 !$OMP PARALLEL DO DEFAULT(SHARED) PRIVATE(ie,iside)
         do ie=1,nelt
@@ -138,10 +138,10 @@
               do i=1,lx1
                 cona = cona +  &
      &          pdiff(i,j,k,ie)*pdiffp(i,j,k,ie)*tmult(i,j,k,ie)
-              end do 
-             end do 
-          end do 
-        end do 
+              end do
+             end do
+          end do
+        end do
 !$OMP END DO nowait
 !$OMP DO
         do im = 1, nmor
@@ -155,15 +155,15 @@
         cona = rho1/cona
 !.......compute (am pm)
         call adds2m1(t,    pdiff,   cona, ntot)
-        call adds2m1(umor, pmorx,   cona, nmor) 
+        call adds2m1(umor, pmorx,   cona, nmor)
 !.......compute r_m+1
         call adds2m1(trhs, pdiffp, -cona, ntot)
-        call adds2m1(rmor, ppmor,  -cona, nmor) 
- 
+        call adds2m1(rmor, ppmor,  -cona, nmor)
+
       end do
 
       if (timeron) call timer_start(t_transf)
-      call transf(umor,t)  
+      call transf(umor,t)
       if (timeron) call timer_stop(t_transf)
       if (timeron) call timer_stop(t_diffusion)
 
@@ -183,23 +183,23 @@
       double precision r(lx1,lx1,lx1), u(lx1,lx1,lx1), rdtime
       integer i,j,k, ix,iz, sizei
 
-      double precision tm1(lx1,lx1,lx1),tm2(lx1,lx1,lx1)                     
+      double precision tm1(lx1,lx1,lx1),tm2(lx1,lx1,lx1)
 
       rdtime = 1.d0/dtime
 
       call r_init(tm1,nxyz,0.d0)
-      do iz=1,lx1                     
+      do iz=1,lx1
         do k = 1, lx1
           do j = 1, lx1
             do i = 1, lx1
               tm1(i,j,iz) = tm1(i,j,iz)+wdtdr(i,k)*u(k,j,iz)
             end do
           end do
-        end do                           
+        end do
       end do
-              
-      call r_init(tm2,nxyz,0.d0)                                                   
-      do iz=1,lx1                                            
+
+      call r_init(tm2,nxyz,0.d0)
+      do iz=1,lx1
         do k = 1, lx1
           do j = 1, lx1
             do i = 1, lx1
@@ -208,10 +208,10 @@
           end do
         end do
       end do
-                                                            
-      call r_init(r,nxyz,0.d0)   
+
+      call r_init(r,nxyz,0.d0)
       do k = 1, lx1
-        do iz=1, lx1    
+        do iz=1, lx1
           do j = 1, lx1
             do i = 1, lx1
               r(i,j,iz) = r(i,j,iz)+u(i,j,k)*wdtdr(k,iz)
@@ -220,13 +220,13 @@
         end do
       end do
 
-!.....collocate with remaining weights and sum to complete factorization.                   
-                                                      
-!      do ix=1,nxyz                                            
+!.....collocate with remaining weights and sum to complete factorization.
+
+!      do ix=1,nxyz
 !         r(ix,1,1)=visc*(tm1(ix,1,1)*g4m1_s(ix,1,1,sizei)+
 !     &                   tm2(ix,1,1)*g5m1_s(ix,1,1,sizei)+
 !     &                     r(ix,1,1)*g6m1_s(ix,1,1,sizei))+
-!     &               bm1_s(ix,1,1,sizei)*rdtime*u(ix,1,1)             
+!     &               bm1_s(ix,1,1,sizei)*rdtime*u(ix,1,1)
 !      end do
       do k=1,lx1
         do j=1,lx1
@@ -234,13 +234,13 @@
             r(i,j,k)=visc*(tm1(i,j,k)*g4m1_s(i,j,k,sizei)+  &
      &                   tm2(i,j,k)*g5m1_s(i,j,k,sizei)+  &
      &                    r(i,j,k)*g6m1_s(i,j,k,sizei))+  &
-     &               bm1_s(i,j,k,sizei)*rdtime*u(i,j,k)             
+     &               bm1_s(i,j,k,sizei)*rdtime*u(i,j,k)
           end do
         end do
       end do
 
-      return                                                  
-      end                                                    
+      return
+      end
 
 
- 
+
